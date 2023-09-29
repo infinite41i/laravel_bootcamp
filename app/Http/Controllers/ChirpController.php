1c077571fe3da28e5b509166f8d4b145b1760cdf
alitohidi <alton3223@gmail.com>
2023-09-29 10:29:21 +0330

    feat: creating chirps

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ChirpController extends Controller
+{
+    function index(): View {
+        return view('chirps', [
+            //
+        ]);
+    }
+}
